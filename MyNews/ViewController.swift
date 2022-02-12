cec9b0e9d247dcafc8f1b5aa1eab3cc70f35e5a7
Ernest Nyumbu <ernestnyumbu@gmail.com>
2022-02-12 14:15:15 +0200

    Installed required pods, setup Firebase and added fonts.

---
+++ MyNews/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyNews
+//
+//  Created by Ernest Nyumbu on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let button = UIButton(type: .roundedRect)
+          button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+          button.setTitle("Test Crash", for: [])
+          button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+          view.addSubview(button)
+    }
+
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+          let numbers = [0]
+          let _ = numbers[1]
+      }
+}
+
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'MyNews' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MyNews
+  pod 'lottie-ios'
+  pod 'Kingfisher', '~> 7.0'
+  pod 'Firebase/Crashlytics'
+  pod 'Firebase/Analytics'
+
+  target 'MyNewsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MyNewsUITests' do
+    # Pods for testing
+  end
+
+end
